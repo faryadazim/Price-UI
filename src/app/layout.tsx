d555e2738403367ce9bb057a58dacc843168a434
Faryad Azim <faryadmaalic92@yahoo.com>
2023-03-14 08:08:51 +0500

    ui completed just mobile reponsivness remain will be completed today

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,20 @@
 'use client';
 import './globals.css' 
+import { ChakraProvider } from '@chakra-ui/react' 
+import { CacheProvider } from '@chakra-ui/next-js'
 
-
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
-
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: {  children: React.ReactNode }) {
   return (
     <html lang="en">
      
       <head />
      
       <body>
+      {/* <CacheProvider> */}
       <ChakraProvider>
       {children}
     </ChakraProvider>
+    {/* </CacheProvider> */}
 </body>
     </html>
   )
